bee6eab552eef86639a323aa90243ff4a9fd6c51
Ryan Walberg <rwalberg@slack-corp.com>
2023-12-01 09:43:05 -0500

    finished day 1

--- src/day01.cpp
+++ src/day01.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    ifstream file;
+    file.open("../data/day01.txt");
+    if (!file.is_open())
+    {
+        cerr << "could not open data file." << endl;
+        return 1;
+    }
+
+    string line;
+    long sum = 0;
+    while (getline(file, line))
+    {
+        char firstDigit;
+        char lastDigit;
+        for (int i = 0; i < line.length(); i++) {
+            if (isnumber(line[i])){
+                firstDigit = line[i] - '0';
+                break;
+            }
+        }
+        for (int i = line.length() - 1; i >= 0; i--) {
+            if (isnumber(line[i])){
+                lastDigit = line[i] - '0';
+                break;
+            }
+        }
+        sum += 10 * firstDigit + lastDigit;
+    }
+    file.close();
+
+    cout << "sum is " << sum << endl;
 }
